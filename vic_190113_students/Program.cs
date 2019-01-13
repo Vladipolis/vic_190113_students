49595c78ad442ca08c7b171411adb28ca2b89352
Vladipolis <dont_contact_me@mail.ru>
2019-01-13 12:59:27 +0500

    Add project files.

---
+++ vic_190113_students/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vic_190113_students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
